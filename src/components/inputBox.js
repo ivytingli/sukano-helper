ab19887a51018b02b49148a7587ddbaf3c915062
Ivy Li <ivy.ting.li@gmail.com>
2019-06-07 22:29:36 -0400

    Input box styling

--- src/components/inputBox.js
+++ src/components/inputBox.js
@@ -9,15 +9,6 @@ const numberGridStyle = {
   fontSize: "8pt"
 };
 
-const outlineStyle = {
-  width: "39px",
-  height: "39px",
-  backgroundColor: "white",
-  border: "1px solid black",
-  margin: "3px",
-  float: "left"
-};
-
 const displayNumber = (value, arr) => {
   let display;
   arr && arr.includes(value) ? (display = value) : (display = " ");
@@ -26,21 +17,23 @@ const displayNumber = (value, arr) => {
 
 const InputBox = props => {
   return (
-    <div
-      style={outlineStyle}
-      tabIndex={props.canFocus}
-      onKeyDown={props.handleKeyPress}
-    >
-      <div style={numberGridStyle}>{displayNumber(1, props.candidates)}</div>
-      <div style={numberGridStyle}>{displayNumber(2, props.candidates)}</div>
-      <div style={numberGridStyle}>{displayNumber(3, props.candidates)}</div>
-      <div style={numberGridStyle}>{displayNumber(4, props.candidates)}</div>
-      <div style={numberGridStyle}>{displayNumber(5, props.candidates)}</div>
-      <div style={numberGridStyle}>{displayNumber(6, props.candidates)}</div>
-      <div style={numberGridStyle}>{displayNumber(7, props.candidates)}</div>
-      <div style={numberGridStyle}>{displayNumber(8, props.candidates)}</div>
-      <div style={numberGridStyle}>{displayNumber(9, props.candidates)}</div>
-    </div>
+    <span>
+      <span className="input-box input-box-pos">
+        <div style={numberGridStyle}>{displayNumber(1, props.candidates)}</div>
+        <div style={numberGridStyle}>{displayNumber(2, props.candidates)}</div>
+        <div style={numberGridStyle}>{displayNumber(3, props.candidates)}</div>
+        <div style={numberGridStyle}>{displayNumber(4, props.candidates)}</div>
+        <div style={numberGridStyle}>{displayNumber(5, props.candidates)}</div>
+        <div style={numberGridStyle}>{displayNumber(6, props.candidates)}</div>
+        <div style={numberGridStyle}>{displayNumber(7, props.candidates)}</div>
+        <div style={numberGridStyle}>{displayNumber(8, props.candidates)}</div>
+        <div style={numberGridStyle}>{displayNumber(9, props.candidates)}</div>
+      </span>
+      <input
+        className="input-box input-cursor"
+        onKeyDown={props.handleKeyPress}
+      />
+    </span>
   );
 };
 
